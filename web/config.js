5ac3e53d401eedb4bee27aae6fa70c0ac13710e7
OSL Creator <ward.mtsu@gmail.com>
2026-04-13 18:47:12 -0500

    Fix Pages config to use demo mode

--- web/config.js
+++ web/config.js
@@ -1,6 +1,5 @@
 window.OSL_CONFIG = {
   API_BASE: window.location.origin.includes('github.io')
     ? null
-    : window.location.origin,
-  DEMO_MODE: window.location.origin.includes('github.io'),
+    : window.location.origin
 };
